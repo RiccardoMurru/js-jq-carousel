3bf1cf10db5e69a9b8881926bd279b5ec2c82722
Riccardo Murru <riccardo.mr@gmail.com>
2020-04-09 18:30:56 +0200

    add. image counter on bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,22 +7,34 @@
 
  $(document).ready(function() {
 
-    var img = $('img');
     var imgFirst = $('.first');
     var imgLast = $('.last');
     var arrowRight = $('.arrow-right');
     var arrowLeft = $('.arrow-left');
+    var counterFirst = $('.first-counter');
+    var counterLast = $('.last-counter');
 
 
     // cambio immagine a click su freccia avanti
     arrowRight.click(function() {
 
         var activeImage = $('.active');
+        var activeCounter = $('.active-counter')
+
+        // scorrimento immagini
         activeImage.toggleClass('active');
         activeImage.next('img').toggleClass('active');
 
         if (activeImage.hasClass('last')) {
             imgFirst.toggleClass('active');
+        }
+
+        // scorrimento contatore
+        activeCounter.toggleClass('active-counter');
+        activeCounter.next('li').toggleClass('active-counter');
+
+        if (activeCounter.hasClass('last-counter')) {
+            counterFirst.toggleClass('active-counter');
         }
     });
 
@@ -30,11 +42,22 @@
     arrowLeft.click(function() {
         
         var activeImage = $('.active');
+        var activeCounter = $('.active-counter')
+
+        // scorrimento immagini
         activeImage.toggleClass('active');
         activeImage.prev('img').toggleClass('active');
 
         if (activeImage.hasClass('first')) {
             imgLast.toggleClass('active');
+        }
+
+        // scorrimento contatore
+        activeCounter.toggleClass('active-counter');
+        activeCounter.prev('li').toggleClass('active-counter');
+
+        if (activeCounter.hasClass('first-counter')) {
+            counterLast.toggleClass('active-counter');
         }
         
     });
